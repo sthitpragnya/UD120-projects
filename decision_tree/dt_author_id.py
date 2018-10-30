1838471d6b5004dd81c835b053e0c49b972be4cc
smit <smit.patoliya92@gmail.com>
2018-10-31 01:47:33 +0800

    Completed Decision Tree mini-project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split = 40)
+t0 = time()
+clf.fit(features_train, labels_train)
+t1 = time()
+print "Model fitting time:", round(t1-t0, 3), "s"
+pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t1, 3), "s"
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
 
 #########################################################
 
