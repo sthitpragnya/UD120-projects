b39ddd9466535c5f94afcccf7ac850c6711c3110
smit <smit.patoliya92@gmail.com>
2018-11-01 03:55:34 +0800

    Completed outliers mini-project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,15 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
     cleaned_data = []
 
     ### your code goes here
-
+    for i in range(len(predictions)):
+        cleaned_data.append((ages[i], net_worths[i], abs(net_worths[i] - predictions[i])))
+    
+    cleaned_data.sort(key=lambda tup: tup[2], reverse = True)
+    
+    del cleaned_data[0:9]
     
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,7 +25,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print "R-squared (train):", reg.score(ages_train, net_worths_train)
+print "R-squared (test):", reg.score(ages_test, net_worths_test)
+print "Slope:", reg.coef_
+print "Intercept:", reg.intercept_
 
 
 
@@ -68,7 +74,13 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "######### Cleaned Data #########"
+        print "R-squared (train):", reg.score(ages, net_worths)
+        print "R-squared (test):", reg.score(ages_test, net_worths_test)
+        print "Slope:", reg.coef_
+        print "Intercept:", reg.intercept_
         plt.plot(ages, reg.predict(ages), color="blue")
+        
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
